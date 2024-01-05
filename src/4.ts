1e03eea400885c1f85fdc54c40534db96e77d407
k0robova <alonakorobova@ukr.net>
2024-01-05 16:09:34 +0200

    fixed

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,46 @@
-const key = new Key();
+// Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте відтворити сценарій, в якому людина приходить додому.
+
+class Key {
+  constructor(private signature: number) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  openDoor(key: Key): void {
+    this.key = key;
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +48,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
